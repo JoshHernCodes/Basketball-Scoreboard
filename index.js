aad03cd6a267c46e0617ca57503ba1eb8ec07efa
JoshHernCodes <joshherncodes@gmail.com>
2022-11-18 15:28:06 -0500

    Format
    reformatted Code

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ let homeScore = 0;
 let awayScore = 0;
 
 function homeAddOne() {
-    homeScore += 1;
-    homeBoard.textContent = homeScore;
+  homeScore += 1;
+  homeBoard.textContent = homeScore;
 }
-function homeAddTwo(){
-    homeScore += 2;
-    homeBoard.textContent = homeScore;
+function homeAddTwo() {
+  homeScore += 2;
+  homeBoard.textContent = homeScore;
 }
-function homeAddThree(){
-    homeScore += 3;
-    homeBoard.textContent = homeScore;
+function homeAddThree() {
+  homeScore += 3;
+  homeBoard.textContent = homeScore;
 }
-function awayAddOne(){
-    awayScore += 1;
-    awayBoard.textContent = awayScore;
+function awayAddOne() {
+  awayScore += 1;
+  awayBoard.textContent = awayScore;
 }
-function awayAddTwo(){
-    awayScore += 2;
-    awayBoard.textContent = awayScore;
+function awayAddTwo() {
+  awayScore += 2;
+  awayBoard.textContent = awayScore;
 }
-function awayAddThree(){
-    awayScore += 3;
-    awayBoard.textContent = awayScore;
+function awayAddThree() {
+  awayScore += 3;
+  awayBoard.textContent = awayScore;
 }
-function newGame(){
-    homeScore = 0;
-    awayScore = 0;
-    homeBoard.textContent = 0;
-    awayBoard.textContent = 0;
+function newGame() {
+  homeScore = 0;
+  awayScore = 0;
+  homeBoard.textContent = 0;
+  awayBoard.textContent = 0;
 }
